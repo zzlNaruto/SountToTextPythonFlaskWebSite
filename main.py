e94455b1da7b205aaa0ce2a5f1587d7b561eed54
zzlNaruto <120311662@qq.com>
2025-01-28 00:27:42 +0800

    目前可以完成上传文件的功能，前端ui交互也还凑合。但是上传后无法直接调用音频转文字的接口，不知道为什么

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for,Response
+from flask import Flask, render_template, request, jsonify, redirect, url_for,Response
 from werkzeug.utils import secure_filename
 import oss2
 from oss2.credentials import EnvironmentVariableCredentialsProvider
 import os,sys
 import time
 import threading
+from sttLogic import fileTrans
+
 
 # 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
 auth = oss2.ProviderAuthV4(EnvironmentVariableCredentialsProvider())
@@ -21,7 +23,7 @@ bucket = oss2.Bucket(auth, endpoint, "stt-bucket", region=region)
 app = Flask(__name__)
 
 # 存储上传进度的字典，避免覆盖问题
-upload_progress = {}
+uploadProgress = {}
 
 # 启动一个线程锁，确保线程安全
 lock = threading.Lock()
@@ -31,38 +33,55 @@ def home():
     return render_template('index.html')
 
 @app.route('/uploadFile', methods=['POST'])
-def upload_file():
+def uploadFile():
     if 'audio_file' not in request.files:
         return "No file part", 400
     file = request.files['audio_file']
     if file.filename == '':
         return "No selected file", 400
     filename = secure_filename(file.filename)
+    # 根据按钮的 value 执行不同操作
+    print(request.form)  # 打印接收到的表单数据
+    action = request.form.get('action')
+    print('action:', action)
 
-    # 每个文件都拥有独立的进度信息
-    with lock:
-        upload_progress[filename] = {'percentage': 0}
-
-    def save_file_with_progress():
-        # consumed_bytes表示已上传的数据量。
-        # total_bytes表示待上传的总数据量。当无法确定待上传的数据长度时，total_bytes的值为None。
-        def percentage(consumed_bytes, total_bytes):
-            if total_bytes:
-                rate = int(100 * (float(consumed_bytes) / float(total_bytes)))
-                # 更新文件上传进度
-                with lock:
-                    upload_progress[filename]['percentage'] = rate
-                sys.stdout.flush()
-        # 使用文件对象上传到 OSS
-        file_data = file.read()
-        bucket.put_object('sttFile/'+filename, file_data,progress_callback=percentage)
-        # 上传完成后，设置上传进度为100
+    if action == 'bottonSoundToText':
+        print('bottonSoundToText')
+        # 每个文件都拥有独立的进度信息
         with lock:
-            upload_progress[filename]['percentage'] = 100
+            uploadProgress[filename] = {'percentage': 0}
 
-    # 启动后台线程进行文件上传
-    threading.Thread(target=save_file_with_progress, daemon=True).start()
-    return 'File uploaded successfully'
+        def saveFileWithProgress():
+            # consumed_bytes表示已上传的数据量。
+            # total_bytes表示待上传的总数据量。当无法确定待上传的数据长度时，total_bytes的值为None。
+            def percentage(consumed_bytes, total_bytes):
+                if total_bytes:
+                    rate = int(100 * (float(consumed_bytes) / float(total_bytes)))
+                    # 更新文件上传进度
+                    with lock:
+                        uploadProgress[filename]['percentage'] = rate
+                    sys.stdout.flush()
+            # 使用文件对象上传到 OSS
+            fileData = file.read()
+            bucket.put_object('sttFile/'+filename, fileData,progress_callback=percentage)
+            # 上传完成后，设置上传进度为100
+            with lock:
+                uploadProgress[filename]['percentage'] = 100
+
+        # 启动后台线程进行文件上传
+        threading.Thread(target=saveFileWithProgress, daemon=True).start()
+        # 音频转文字在这里调用有问题，需要在网页端接收到上传完成后的状态再调用
+        return jsonify({'message': 'File uploaded successfully', 'action': 'bottonSoundToText', 'code': 'S', 'filename': filename}), 200
+    elif action == 'bottonRegenerate':
+        # 在这里添加语音转文字的逻辑
+        print('bottonRegenerate')
+        returnJson = fileTrans(filename)
+        convertedText = returnJson.payload
+        print(convertedText)
+        return returnJson, 200
+    else:
+        print('Invalid')
+        return "Invalid action", 400
 
 @app.route('/progress')
 def progress():
@@ -73,7 +92,7 @@ def progress():
         while True:
             with lock:
                 # 只推送每个文件的上传进度
-                for filename, progress_data in upload_progress.items():
+                for filename, progress_data in uploadProgress.items():
                     print('\r{0}% '.format(progress_data['percentage']), end='')
                     yield f"data: {{\"file\": \"{filename}\", \"percentage\": {progress_data['percentage']}}}\n\n"
             time.sleep(0.5)  # 每秒钟推送一次进度
@@ -82,3 +101,24 @@ def progress():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+@app.route('/soundToText', methods=['POST'])
+def soundToText():
+    data = request.json
+    print(data)
+    if not data:
+        return jsonify({'error': 'Invalid JSON data'}), 400
+
+    # 提取具体的键值
+    filename = data.get('filename')
+
+    # 返回处理后的响应
+    # returnText = fileTrans(filename)
+    returnText = jsonify({
+                            'message': 'Success', 'payload': 'contentText', 'code': 'S'
+                        })
+    if returnText.code == 'S':
+        # print(returnText)
+        return returnText, 200
+    elif returnText.code == 'E':
+        return returnText, 400
---
+++ sttLogic.py
@@ -0,0 +1,142 @@
+# -*- coding: utf8 -*-
+import os
+import json
+import time
+import oss2
+from oss2.credentials import EnvironmentVariableCredentialsProvider
+import requests
+from aliyunsdkcore.acs_exception.exceptions import ClientException
+from aliyunsdkcore.acs_exception.exceptions import ServerException
+from aliyunsdkcore.client import AcsClient
+from aliyunsdkcore.request import CommonRequest
+from flask import jsonify
+def fileTrans(object_name) :
+    # 地域ID，固定值。
+    REGION_ID = "cn-beijing"
+    PRODUCT = "nls-filetrans"
+    DOMAIN = "filetrans.cn-beijing.aliyuncs.com"
+    API_VERSION = "2018-08-17"
+    POST_REQUEST_ACTION = "SubmitTask"
+    GET_REQUEST_ACTION = "GetTaskResult"
+    # 请求参数
+    KEY_APP_KEY = "appkey"
+    KEY_FILE_LINK = "file_link"
+    KEY_VERSION = "version"
+    KEY_ENABLE_WORDS = "enable_words"
+    # 是否开启智能分轨
+    KEY_AUTO_SPLIT = "auto_split"
+    # 响应参数
+    KEY_TASK = "Task"
+    KEY_TASK_ID = "TaskId"
+    KEY_STATUS_TEXT = "StatusText"
+    KEY_RESULT = "Result"
+    KEY_SENTENCES = "Sentences"
+    KEY_TEXT = "Text"
+    # 状态值
+    STATUS_SUCCESS = "SUCCESS"
+    STATUS_RUNNING = "RUNNING"
+    STATUS_QUEUEING = "QUEUEING"
+
+    accessKeyId = os.getenv("OSS_ACCESS_KEY_ID")
+    accessKeySecret = os.getenv("OSS_ACCESS_KEY_SECRET")
+
+    # 创建AcsClient实例
+    client = AcsClient(accessKeyId, accessKeySecret, REGION_ID)
+    # 提交录音文件识别请求
+    postRequest = CommonRequest()
+    postRequest.set_domain(DOMAIN)
+    postRequest.set_version(API_VERSION)
+    postRequest.set_product(PRODUCT)
+    postRequest.set_action_name(POST_REQUEST_ACTION)
+    postRequest.set_method('POST')
+
+    # 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+    auth = oss2.ProviderAuth(EnvironmentVariableCredentialsProvider())
+    # 填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
+    endpoint = "https://oss-cn-beijing.aliyuncs.com"
+
+    # 填写Endpoint对应的Region信息，例如cn-hangzhou。注意，v4签名下，必须填写该参数
+    region = "cn-beijing"
+
+    # yourBucketName填写存储空间名称。
+    bucket_name = "stt-bucket"
+
+    bucket = oss2.Bucket(auth, endpoint, bucket_name, region=region)
+
+    appKey = os.getenv("STT_APP_KEY")
+
+    # 生成下载文件的签名URL，有效时间为3600秒。
+    # 设置slash_safe为True，OSS不会对Object完整路径中的正斜线（/）进行转义，此时生成的签名URL可以直接使用。
+    fileUrl = bucket.sign_url('GET', object_name, 3600, slash_safe=True)
+
+    # fileUrl = "https://stt-bucket.oss-cn-beijing.aliyuncs.com/helloworld/nls-sample-16k.wav?Expires=1733845668&OSSAccessKeyId=TMP.3KfozSkfUsJXDLc784yEQ3sqTuo14kv2YmjAD6veRVmNuajA7deYLW9rvjsMdj461JnW4Rz3U1Ygvmm6iXCGy295AxMqqA&Signature=I67YF2f1NLo13RP3Or91M3b2Sm4%3D"
+    # fileUrl = "https://stt-bucket.oss-cn-beijing-internal.aliyuncs.com/helloworld/nls-sample-16k.wav"
+
+    # 新接入请使用4.0版本，已接入（默认2.0）如需维持现状，请注释掉该参数设置。
+    # 设置是否输出词信息，默认为false，开启时需要设置version为4.0。
+    task = {KEY_APP_KEY : appKey, KEY_FILE_LINK : fileUrl, KEY_VERSION : "4.0", KEY_ENABLE_WORDS : False}
+    # 开启智能分轨，如果开启智能分轨，task中设置KEY_AUTO_SPLIT为True。
+    # task = {KEY_APP_KEY : appKey, KEY_FILE_LINK : fileLink, KEY_VERSION : "4.0", KEY_ENABLE_WORDS : False, KEY_AUTO_SPLIT : True}
+    task = json.dumps(task)
+    print(task)
+    postRequest.add_body_params(KEY_TASK, task)
+    taskId = ""
+    try :
+        postResponse = client.do_action_with_exception(postRequest)
+        postResponse = json.loads(postResponse)
+        print (postResponse)
+        statusText = postResponse[KEY_STATUS_TEXT]
+        if statusText == STATUS_SUCCESS :
+            print ("录音文件识别请求成功响应！")
+            taskId = postResponse[KEY_TASK_ID]
+        else :
+            print ("录音文件识别请求失败！")
+            return
+    except ServerException as e:
+        print (e)
+    except ClientException as e:
+        print (e)
+    # 创建CommonRequest，设置任务ID。
+    getRequest = CommonRequest()
+    getRequest.set_domain(DOMAIN)
+    getRequest.set_version(API_VERSION)
+    getRequest.set_product(PRODUCT)
+    getRequest.set_action_name(GET_REQUEST_ACTION)
+    getRequest.set_method('GET')
+    getRequest.add_query_param(KEY_TASK_ID, taskId)
+    # 提交录音文件识别结果查询请求
+    # 以轮询的方式进行识别结果的查询，直到服务端返回的状态描述符为"SUCCESS"、"SUCCESS_WITH_NO_VALID_FRAGMENT"，
+    # 或者为错误描述，则结束轮询。
+    statusText = ""
+    while True :
+        try :
+            getResponse = client.do_action_with_exception(getRequest)
+            getResponse = json.loads(getResponse)
+            print (getResponse)
+            statusText = getResponse[KEY_STATUS_TEXT]
+            if statusText == STATUS_RUNNING or statusText == STATUS_QUEUEING :
+                # 继续轮询
+                time.sleep(10)
+            else :
+                # 退出轮询
+                break
+        except ServerException as e:
+            print (e)
+        except ClientException as e:
+            print (e)
+    if statusText == STATUS_SUCCESS :
+        print ("录音文件识别成功！")
+        contentText = getResponse[KEY_RESULT][KEY_SENTENCES][0][KEY_TEXT]
+        print (contentText)
+        return jsonify({'message': 'Success', 'payload': contentText, 'code': 'S'})
+    else :
+        print ("录音文件识别失败！")
+        return jsonify({'message': 'Error', 'payload': contentText, 'code': 'E'})
+    return
+
+# 填写Object完整路径，例如exampledir/exampleobject.txt。Object完整路径中不能包含Bucket名称。
+object_name = 'helloworld/nls-sample-16k.wav'
+
+
+# 执行录音文件识别
+# fileTrans(object_name)
